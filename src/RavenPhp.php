9045ee27ac5546b1e523333ac51f88c7ce131246
Victor Sandoval <vsandoval@codersinc.net>
2024-04-17 08:15:40 -0500

    Raven php implementation

---
+++ src/RavenPhp.php
@@ -0,0 +1,25 @@
+<?php
+namespace Lideres\RavenPhp;
+
+class RavenPhp
+{
+    public function transform(string $path): bool | string
+    {
+        $path = escapeshellarg($path);
+        $dir = __DIR__;
+        $command = "";
+        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+            $command = str_replace("\src", "\bin\main.exe $path", $dir);
+        } else {
+            $command = str_replace("\src", "\bin\main $path", $dir);
+        }
+
+        try{
+            $output = array();
+            exec($command, $output);
+            return str_contains('successfully', $output[0]);
+        }catch (\Throwable $e) {
+            return $e->getMessage();
+        }
+    }
+}

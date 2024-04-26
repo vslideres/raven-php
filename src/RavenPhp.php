f13ba0e73dc0e5b018a1f403e0123a1ecbcf226c
Victor Sandoval <vsandoval@codersinc.net>
2024-04-26 06:44:41 -0500

    fix linux

--- src/RavenPhp.php
+++ src/RavenPhp.php
@@ -3,15 +3,15 @@ namespace Lideres\RavenPhp;
 
 class RavenPhp
 {
-    public function transform(string $path): bool | string
+    public function transform(string $path, string $outputPath = null): bool | string
     {
         $path = escapeshellarg($path);
         $dir = __DIR__;
         $command = "";
         if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-            $command = str_replace("\src", "\bin\main.exe $path", $dir);
+            $command = str_replace("\src", "\bin\main.exe $path $outputPath", $dir);
         } else {
-            $command = str_replace("\src", "\bin\main $path", $dir);
+            $command = str_replace("\src", "\bin\main $path $outputPath", $dir);
         }
 
         try{
@@ -19,6 +19,7 @@ class RavenPhp
             exec($command, $output);
             return str_contains('successfully', $output[0]);
         }catch (\Throwable $e) {
+
             return $e->getMessage();
         }
     }

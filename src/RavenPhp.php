5ceccb8e3ba62bb86d59cb7ccc2d23665446d871
Victor Sandoval <vsandoval@codersinc.net>
2024-04-26 08:47:10 -0500

    fix linux

--- src/RavenPhp.php
+++ src/RavenPhp.php
@@ -11,7 +11,7 @@ class RavenPhp
         if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
             $command = str_replace("\src", "\bin\main.exe '$path' '$outputPath'", $dir);
         } else {
-            $command = str_replace("\src", "\bin\main $path '$outputPath'", $dir);
+            $command = str_replace("\src", "\bin\main '$path' '$outputPath'", $dir);
         }
 
         try{
